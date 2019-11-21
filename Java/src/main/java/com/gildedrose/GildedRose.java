0a030152576077b559fd169c64ffb2b96a53c6eb
Görge Albrecht <goerge.@code-mentor.com>
2019-11-21 13:10:10 +0100

    refactorings

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -1,62 +1,86 @@
 package com.gildedrose;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Consumer;
+
 class GildedRose {
-    Item[] items;
+  private static final String AGED_BRIE = "Aged Brie";
+  private static final String BACKSTAGE_PASS = "Backstage passes to a TAFKAL80ETC concert";
+  private static final String SULFURAS = "Sulfuras, Hand of Ragnaros";
 
-    public GildedRose(Item[] items) {
-        this.items = items;
+  private Item[] items;
+
+  public GildedRose(Item[] items) {
+    this.items = items;
+  }
+
+  private static final Map<String, Consumer<Item>> UPDATER = new HashMap<String, Consumer<Item>>() {
+    {
+      put(AGED_BRIE, GildedRose::updateAgedBrie);
+      put(BACKSTAGE_PASS, GildedRose::updateBackstagePass);
+      put(SULFURAS, GildedRose::updateSulfaras);
     }
 
-    public void updateQuality() {
-        for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                        items[i].quality = items[i].quality - 1;
-                    }
-                }
-            } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
+    @Override
+    public Consumer<Item> get(Object key) {
+      return super.getOrDefault(key, GildedRose::updateDefault);
+    }
+  };
 
-                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
+  public void updateQuality() {
+    for (Item item : items) {
+      UPDATER.get(item.name).accept(item);
+    }
+  }
 
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-                    }
-                }
-            }
+  private static void updateDefault(Item item) {
+    item.sellIn = item.sellIn - 1;
+    if (item.quality > 0) {
+      item.quality = item.quality - 1;
+    }
+    if (item.sellIn < 0) {
+      if (item.quality > 0) {
+        item.quality = item.quality - 1;
+      }
+    }
+  }
 
-            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                items[i].sellIn = items[i].sellIn - 1;
-            }
+  private static void updateAgedBrie(Item item) {
+    item.sellIn = item.sellIn - 1;
+    if (item.quality < 50) {
+      item.quality = item.quality + 1;
+    }
+    if (item.sellIn < 0) {
+      if (item.quality < 50) {
+        item.quality = item.quality + 1;
+      }
+    }
+  }
 
-            if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Aged Brie")) {
-                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                                items[i].quality = items[i].quality - 1;
-                            }
-                        }
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality;
-                    }
-                } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
-                    }
-                }
-            }
+  private static void updateBackstagePass(Item item) {
+    if (item.quality < 50) {
+      item.quality = item.quality + 1;
+
+      if (item.sellIn < 11) {
+        if (item.quality < 50) {
+          item.quality = item.quality + 1;
         }
+      }
+
+      if (item.sellIn < 6) {
+        if (item.quality < 50) {
+          item.quality = item.quality + 1;
+        }
+      }
     }
+    item.sellIn = item.sellIn - 1;
+
+    if (item.sellIn < 0) {
+      item.quality = 0;
+    }
+  }
+
+  private static void updateSulfaras(Item item) {
+  }
 }
